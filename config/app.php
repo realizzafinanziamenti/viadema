8065751ec8b54425347fadaeb752f239bee7d22b
pietro-giordano <giopi87@gmail.com>
2025-04-16 17:23:07 +0200

    app config

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'RealizzaGroup'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,7 +65,9 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'Europe/Rome'),
+
+    'schedule_timezone' => env('APP_SCHEDULE_TIMEZONE', 'Europe/Rome'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,11 +80,11 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'locale' => env('APP_LOCALE', 'it'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'it_IT'),
 
     /*
     |--------------------------------------------------------------------------
