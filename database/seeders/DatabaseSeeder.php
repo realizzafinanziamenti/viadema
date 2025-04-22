676a968f894e606e9c540964b5f9567bc86c33bc
pietro-giordano <giopi87@gmail.com>
2025-04-22 12:28:09 +0200

    add user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             RolesAndPermissionsSeeder::class,
             SuperAdminSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use App\Models\UserProfile;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        /**
+         * Create team member test user
+         */
+        $teamMemberTest = User::factory()->create([
+            'first_name' => 'Team Member',
+            'last_name' => 'Test',
+            'email' => 'team@test.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $teamMemberTest->assignRole('team_member');
+
+        UserProfile::factory()->create([
+            'user_id' => $teamMemberTest->id,
+            'phone' => '1234567890',
+            'tax_id' => 'TMMTST12X34Y123Z',
+            'date_of_birth' => '1990-01-01',
+            'city' => 'Roma',
+        ]);
+
+        /**
+         * Create random team members
+         */
+        for ($i = 0; $i < 25; $i++) {
+            $teamMember = User::factory()->create();
+
+            $teamMember->assignRole('team_member');
+
+            UserProfile::factory()->create([
+                'user_id' => $teamMember->id,
+            ]);
+        }
+    }
+}
