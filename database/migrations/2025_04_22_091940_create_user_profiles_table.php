91ae921cde137b62d4e73cf259998a2a52b7bf5b
pietro-giordano <giopi87@gmail.com>
2025-04-22 16:32:19 +0200

    fix profile migration

--- database/migrations/2025_04_22_091940_create_user_profiles_table.php
+++ database/migrations/2025_04_22_091940_create_user_profiles_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->date('date_of_birth')->nullable();
             $table->string('city')->nullable();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
