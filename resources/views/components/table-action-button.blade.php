67cebade344a2245c000a5c4f27456b63c853de3
pietro-giordano <giopi87@gmail.com>
2025-04-23 09:41:30 +0200

    fix table button

--- resources/views/components/table-action-button.blade.php
+++ resources/views/components/table-action-button.blade.php
@@ -1,7 +1,7 @@
 @props(['type' => 'button'])
 
 <button type="{{ $type }}"
-    {{ $attributes->merge(['class' => 'flex items-center justify-center rounded-full text-gray-custom-4 bg-gray-custom-1 h-8 w-8']) }}>
+    {{ $attributes->merge(['class' => 'flex items-center justify-center rounded-full text-gray-custom-4 bg-gray-custom-1 h-8 w-8 cursor-pointer']) }}>
 
     {{ $slot }}
 </button>
