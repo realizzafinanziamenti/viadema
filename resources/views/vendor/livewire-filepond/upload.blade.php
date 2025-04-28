31775701f005902cabc4e24cab459f9cdb63ba6c
pietro-giordano <giopi87@gmail.com>
2025-04-28 14:57:20 +0200

    hidden filepond credits

--- resources/views/vendor/livewire-filepond/upload.blade.php
+++ resources/views/vendor/livewire-filepond/upload.blade.php
@@ -1,5 +1,5 @@
 @php
-$isCustomPlaceholder = isset($placeholder);
+    $isCustomPlaceholder = isset($placeholder);
 @endphp
 
 @props([
@@ -10,104 +10,98 @@ $isCustomPlaceholder = isset($placeholder);
 ])
 
 @php
-if (! $wireModelAttribute = $attributes->whereStartsWith('wire:model')->first()) {
-    throw new Exception("You must wire:model to the filepond input.");
-}
+    if (!($wireModelAttribute = $attributes->whereStartsWith('wire:model')->first())) {
+        throw new Exception('You must wire:model to the filepond input.');
+    }
 
-$pondProperties = $attributes->except([
-    'class',
-    'placeholder',
-    'required',
-    'disabled',
-    'multiple',
-    'wire:model',
-]);
+    $pondProperties = $attributes->except(['class', 'placeholder', 'required', 'disabled', 'multiple', 'wire:model']);
 
-// convert keys from kebab-case to camelCase
-$pondProperties = collect($pondProperties)
-    ->mapWithKeys(fn ($value, $key) => [Illuminate\Support\Str::camel($key) => $value])
-    ->toArray();
+    // convert keys from kebab-case to camelCase
+    $pondProperties = collect($pondProperties)
+        ->mapWithKeys(fn($value, $key) => [Illuminate\Support\Str::camel($key) => $value])
+        ->toArray();
 
-$pondLocalizations = __('livewire-filepond::filepond');
+    $pondLocalizations = __('livewire-filepond::filepond');
 @endphp
 
-<div
-    class="{{ $attributes->get('class') }}"
-    wire:ignore
-    x-cloak
-    x-data="{
-        model: @entangle($wireModelAttribute),
-        isMultiple: @js($multiple),
-        current: undefined,
-        files: [],
-        async loadModel() {
-            if (! this.model) {
-              return;
-            }
-
-            if (this.isMultiple) {
-              await Promise.all(Object.values(this.model).map(async (picture) => this.files.push(await URLtoFile(picture))))
-              return;
-            }
-
-            this.files.push(await URLtoFile(this.model))
+<div class="{{ $attributes->get('class') }}" wire:ignore x-cloak x-data="{
+    model: @entangle($wireModelAttribute),
+    isMultiple: @js($multiple),
+    current: undefined,
+    files: [],
+    async loadModel() {
+        if (!this.model) {
+            return;
         }
-    }"
-    x-init="async () => {
-      await loadModel();
 
-      const pond = LivewireFilePond.create($refs.input);
+        if (this.isMultiple) {
+            await Promise.all(Object.values(this.model).map(async (picture) => this.files.push(await URLtoFile(picture))))
+            return;
+        }
 
-      pond.setOptions({
-          allowMultiple: isMultiple,
-          server: {
-              process: async (fieldName, file, metadata, load, error, progress) => {
-                  $dispatch('filepond-upload-started', '{{ $wireModelAttribute }}');
-                  await @this.upload('{{ $wireModelAttribute }}', file, async (response) => {
-                    let validationResult  = await @this.call('validateUploadedFile', response);
-                        // Check server validation result
-                        if (validationResult === true) {
-                            // File is valid, dispatch the upload-finished event
-                            load(response);
-                            $dispatch('filepond-upload-finished', { '{{ $wireModelAttribute }}': response });
-                        } else {
-                            // Throw error after validating server side
-                            error('Filepond Api Ignores This Message');
-                            $dispatch('filepond-upload-reset', '{{ $wireModelAttribute }}');
-                        }
-                  }, error, progress);
-              },
-              revert: async (filename, load) => {
-                  await @this.revert('{{ $wireModelAttribute }}', filename, load);
-                  $dispatch('filepond-upload-reverted', {'attribute' : '{{ $wireModelAttribute }}'});
-              },
-              remove: async (file, load) => {
-                  await @this.remove('{{ $wireModelAttribute }}', file.name);
-                  load();
-                  $dispatch('filepond-upload-file-removed', {'attribute' : '{{ $wireModelAttribute }}'});
-              },
-          },
-          required: @js($required),
-          disabled: @js($disabled),
-      });
+        this.files.push(await URLtoFile(this.model))
+    }
+}" x-init="async () => {
+    await loadModel();
 
-      pond.setOptions(@js($pondLocalizations));
+    const pond = LivewireFilePond.create($refs.input);
 
-      pond.setOptions(@js($pondProperties));
+    pond.setOptions({
+        allowMultiple: isMultiple,
+        server: {
+            process: async (fieldName, file, metadata, load, error, progress) => {
+                $dispatch('filepond-upload-started', '{{ $wireModelAttribute }}');
+                await @this.upload('{{ $wireModelAttribute }}', file, async (response) => {
+                    let validationResult = await @this.call('validateUploadedFile', response);
+                    // Check server validation result
+                    if (validationResult === true) {
+                        // File is valid, dispatch the upload-finished event
+                        load(response);
+                        $dispatch('filepond-upload-finished', { '{{ $wireModelAttribute }}': response });
+                    } else {
+                        // Throw error after validating server side
+                        error('Filepond Api Ignores This Message');
+                        $dispatch('filepond-upload-reset', '{{ $wireModelAttribute }}');
+                    }
+                }, error, progress);
+            },
+            revert: async (filename, load) => {
+                await @this.revert('{{ $wireModelAttribute }}', filename, load);
+                $dispatch('filepond-upload-reverted', { 'attribute': '{{ $wireModelAttribute }}' });
+            },
+            remove: async (file, load) => {
+                await @this.remove('{{ $wireModelAttribute }}', file.name);
+                load();
+                $dispatch('filepond-upload-file-removed', { 'attribute': '{{ $wireModelAttribute }}' });
+            },
+        },
+        required: @js($required),
+        disabled: @js($disabled),
+    });
 
-      @if($isCustomPlaceholder)
-      pond.setOptions({ labelIdle: @js($placeholder) });
-      @endif
+    pond.setOptions(@js($pondLocalizations));
 
-      pond.addFiles(files)
-      pond.on('addfile', (error, file) => {
-          if (error) console.log(error);
-      });
+    pond.setOptions(@js($pondProperties));
 
-      $wire.on('filepond-reset-{{ $wireModelAttribute }}', () => {
-          pond.removeFiles();
-      });
-    }"
->
+    @if($isCustomPlaceholder)
+    pond.setOptions({ labelIdle: @js($placeholder) });
+    @endif
+
+    pond.addFiles(files)
+    pond.on('addfile', (error, file) => {
+        if (error) console.log(error);
+    });
+
+    $wire.on('filepond-reset-{{ $wireModelAttribute }}', () => {
+        pond.removeFiles();
+    });
+}">
     <input type="file" x-ref="input">
+
+    <style>
+        /* remove filepond credits */
+        .filepond--credits {
+            display: none !important;
+        }
+    </style>
 </div>
