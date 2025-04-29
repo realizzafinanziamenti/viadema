4fe1859a53cfdbb422e08914c8847cae133b5341
pietro-giordano <giopi87@gmail.com>
2025-04-29 12:59:50 +0200

    customer index page

---
+++ app/Livewire/Admin/Customer/CustomerIndex.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Admin\Customer;
+
+use App\Models\Customer;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+use Livewire\WithoutUrlPagination;
+use Livewire\WithPagination;
+
+class CustomerIndex extends Component
+{
+    use WithPagination, WithoutUrlPagination;
+
+    public $search = '';
+
+    #[Layout('components.layouts.app')]
+    public function render()
+    {
+        $query = Customer::with('user')
+            ->orderByDesc('updated_at');
+
+        $customers = $query->paginate(15);
+
+        return view('livewire.admin.customer.customer-index', [
+            'customers' => $customers,
+        ]);
+    }
+}
