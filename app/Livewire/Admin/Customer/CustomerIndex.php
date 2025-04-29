37ecf2af1adb414dd5f427785f7b2778dfbdd12f
pietro-giordano <giopi87@gmail.com>
2025-04-29 14:15:55 +0200

    add mount in index

--- app/Livewire/Admin/Customer/CustomerIndex.php
+++ app/Livewire/Admin/Customer/CustomerIndex.php
@@ -3,6 +3,7 @@
 namespace App\Livewire\Admin\Customer;
 
 use App\Models\Customer;
+use Illuminate\Support\Facades\Gate;
 use Livewire\Attributes\Layout;
 use Livewire\Component;
 use Livewire\WithoutUrlPagination;
@@ -13,6 +14,11 @@ class CustomerIndex extends Component
     use WithPagination, WithoutUrlPagination;
 
     public $search = '';
+
+    public function mount()
+    {
+        Gate::authorize('viewAny', Customer::class);
+    }
 
     #[Layout('components.layouts.app')]
     public function render()
