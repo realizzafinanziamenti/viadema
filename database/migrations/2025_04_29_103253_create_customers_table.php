460707363161b1991e5335d8c52a504eb08c40f2
pietro-giordano <giopi87@gmail.com>
2025-04-29 11:43:47 +0200

    customer's migration

---
+++ database/migrations/2025_04_29_103253_create_customers_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete()->cascadeOnUpdate();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('phone', 24);
+            $table->string('email')->unique()->nullable();
+            $table->date('date_of_birth')->nullable();
+            $table->string('tax_id', 16)->unique()->nullable();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('state')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
