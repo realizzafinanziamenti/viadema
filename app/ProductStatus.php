a8dbc4c2ab635c6e3c229b390cca8a59d838ae32
pietro-giordano <giopi87@gmail.com>
2025-05-02 16:31:53 +0200

    product status enum class

---
+++ app/ProductStatus.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App;
+
+enum ProductStatus: int
+{
+    case UNDER_REVIEW = 1;
+    case REJECTED = 2;
+    case APPROVED = 3;
+    case SUSPENDED = 4;
+    case PENDING = 5;
+    case DISBURSED = 6;
+
+    /**
+     * Check if the current status is the same as the given status
+     */
+    public function isStatus(ProductStatus $status): bool
+    {
+        return $this === $status;
+    }
+
+    /**
+     * Get the display label for the current role
+     */
+    public function getLabelText(): string
+    {
+        return match ($this) {
+            self::UNDER_REVIEW => 'Istruttoria',
+            self::REJECTED => 'Respinta',
+            self::APPROVED => 'Deliberata',
+            self::SUSPENDED => 'Sospesa',
+            self::PENDING => 'In Attesa',
+            self::DISBURSED => 'Liquidata',
+        };
+    }
+
+    /**
+     * Get the CSS classes for styling the label based on the role
+     */
+    public function getLabelColor(): string
+    {
+        return match ($this) {
+            self::UNDER_REVIEW => 'Istruttoria',
+            self::REJECTED => 'Respinta',
+            self::APPROVED => 'Deliberata',
+            self::SUSPENDED => 'Sospesa',
+            self::PENDING => 'In Attesa',
+            self::DISBURSED => 'Liquidata',
+        };
+    }
+}
