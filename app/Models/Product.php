0efef1f25a055f2f2015cee4d860c5a1a5dbe2d6
pietro-giordano <giopi87@gmail.com>
2025-05-02 16:48:04 +0200

    product model and migration

---
+++ app/Models/Product.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use App\ProductStatus;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'product_type_id',  // tipo prodotto
+        'product_subtype_id',  // sottotipo prodotto
+        'collaborator_id',  // collaboratore
+        'customer_id',  // cliente
+        'financial_table_id',  // tabella/piano finanziario
+        'insurance_id',  // assicurazione
+        'installment_id',  // numero rate
+        'customer_type_id',  // tipologia cliente
+        'previous_finance',  // finanziaria estinta
+        'inserted_at',  // data di inserimento in sistema
+        'started_at',  // data di apertura pratica
+        'paid_at',  // data liquidazione
+        'extinguished_at',  // data di estinzione
+        'renewable_at',  // data di rinnovo
+        'product_status',  // stato prodotto
+        'rate_amount',  // importo rata
+        'notes',
+        'practice_code',  // id univoco pratica
+    ];
+
+    protected $casts = [
+        'product_status' => ProductStatus::class,
+        'inserted_at' => 'datetime',
+        'started_at' => 'datetime',
+        'paid_at' => 'datetime',
+        'extinguished_at' => 'datetime',
+        'renewable_at' => 'datetime',
+        'rate_amount' => 'decimal:2',
+    ];
+}
