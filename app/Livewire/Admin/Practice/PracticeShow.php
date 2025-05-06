48637dc3bd64a80fac68ca890a938c9911111c64
pietro-giordano <giopi87@gmail.com>
2025-05-06 16:48:04 +0200

    practice details page

---
+++ app/Livewire/Admin/Practice/PracticeShow.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Livewire\Admin\Practice;
+
+use App\Models\Practice;
+use Illuminate\Support\Facades\Gate;
+use Livewire\Attributes\Layout;
+use Livewire\Component;
+
+class PracticeShow extends Component
+{
+    public Practice $practice;
+
+    public function mount($id)
+    {
+        $this->practice = Practice::findOrFail($id);
+        // Gate::authorize('view', $this->practice);
+    }
+
+    #[Layout('components.layouts.app')]
+    public function render()
+    {
+        return view('livewire.admin.practice.practice-show');
+    }
+}
