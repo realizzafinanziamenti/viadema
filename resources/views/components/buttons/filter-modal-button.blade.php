51e065335c05c02a493b3986c94807a93d66c3dd
pietro-giordano <giopi87@gmail.com>
2025-05-13 15:39:26 +0200

    filter modal

--- resources/views/components/buttons/filter-modal-button.blade.php
+++ resources/views/components/buttons/filter-modal-button.blade.php
@@ -1,8 +1,8 @@
-{{-- @props([]) --}}
+@props(['name' => 'filter-modal'])
 
 <flux:button
     {{ $attributes->merge(['class' => 'flex items-center justify-center w-full bg-orange-custom! hover:bg-orange-custom-hover! text-white! border-orange-custom!']) }}
-    x-on:click="$dispatch('open-modal', 'filter-modal')">
+    x-on:click="$dispatch('open-modal', '{{ $name }}')">
     <x-icons.ion-filter />
     Filtra
 </flux:button>
--- resources/views/components/delete-modal.blade.php
+++ resources/views/components/delete-modal.blade.php
@@ -1,27 +1,29 @@
 @props(['name', 'header', 'message', 'function', 'show' => false, 'maxWidth' => 'md'])
 
 <x-modal :name="$name" :show="$show" maxWidth="{{ $maxWidth }}">
-    <div class="text-red-600 border-2 border-red-600 rounded-full flex items-center justify-center w-12 h-12">
-        <x-icons.icon-akar-trash-can />
-    </div>
+    <div class="flex flex-col items-center">
+        <div class="text-red-600 border-2 border-red-600 rounded-full flex items-center justify-center w-12 h-12">
+            <x-icons.icon-akar-trash-can />
+        </div>
 
-    <x-modal-header :label="$header" class="mt-5" />
+        <x-modal-header :label="$header" class="mt-5" />
 
-    <div class="text-sm text-gray-custom-4 text-center mt-5">
-        {!! $message !!}
-    </div>
+        <div class="text-sm text-gray-custom-4 text-center mt-5">
+            {!! $message !!}
+        </div>
 
-    {{-- Buttons --}}
-    <div class="grid justify-items-stretch gap-3 mt-6">
-        <flux:button variant="primary" type="button" size="sm"
-            x-on:click="$dispatch('close-modal', '{{ $name }}')"
-            class="px-10 w-full bg-gray-custom-2 border-gray-custom-2 text-gray-custom-5 hover:bg-gray-custom-3-hover hover:border-gray-custom-3-hover hover:text-white">
-            Annulla
-        </flux:button>
+        {{-- Buttons --}}
+        <div class="grid justify-items-stretch gap-3 mt-6">
+            <flux:button variant="primary" type="button" size="sm"
+                x-on:click="$dispatch('close-modal', '{{ $name }}')"
+                class="px-10 w-full bg-gray-custom-2 border-gray-custom-2 text-gray-custom-5 hover:bg-gray-custom-3-hover hover:border-gray-custom-3-hover hover:text-white">
+                Annulla
+            </flux:button>
 
-        <flux:button variant="primary" type="button" size="sm" wire:click='{{ $function }}'
-            class="px-10 w-full bg-red-600 border-red-600 hover:bg-red-800 hover:border-red-800">
-            Procedi ed elimina
-        </flux:button>
+            <flux:button variant="primary" type="button" size="sm" wire:click='{{ $function }}'
+                class="px-10 w-full bg-red-600 border-red-600 hover:bg-red-800 hover:border-red-800">
+                Procedi ed elimina
+            </flux:button>
+        </div>
     </div>
 </x-modal>
