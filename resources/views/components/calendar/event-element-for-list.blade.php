c5eff9f8b92952ca5ed1ebd483024847821c709c
pietro-giordano <giopi87@gmail.com>
2025-05-16 17:41:35 +0200

    fix event element

--- resources/views/components/calendar/event-element-for-list.blade.php
+++ resources/views/components/calendar/event-element-for-list.blade.php
@@ -1,47 +1,45 @@
 @props(['event'])
 
 <div
-    class="flex items-center justify-between px-4 h-20 bg-white text-sm transition duration-300 ease-in-out border rounded-lg cursor-pointer hover:shadow-lg">
+    class="grid grid-cols-12 px-4 py-3 gap-x-3 bg-white text-sm transition duration-300 ease-in-out border rounded-lg cursor-pointer hover:shadow-lg">
 
-    <div class="flex items-center gap-x-8">
-        {{-- Title --}}
-        <div class="w-[300px]">
-            <h3 class="font-semibold text-black-custom truncate" title="{{ $event->title }}">
-                {{ $event->title }}
-            </h3>
-        </div>
-
-        {{-- Date --}}
-        <div class="flex flex-col items-center text-gray-custom-3">
-            <span class="text-sm font-bold">
-                {{ $event->formatted_start_date }}
-            </span>
-            <span class="text-sm">
-                {{ $event->formatted_start_time }} - {{ $event->formatted_end_time }}
-            </span>
-        </div>
+    {{-- Title --}}
+    <div class="col-span-4 flex items-center">
+        <h3 class="font-semibold text-black-custom truncate" title="{{ $event->title }}">
+            {{ $event->title }}
+        </h3>
     </div>
 
-    <div class="flex items-center gap-x-8">
-        {{-- User Owner --}}
-        <div class="w-[200px]">
-            <x-user-table-data :user="$event->user" />
-        </div>
+    {{-- Date --}}
+    <div class="col-span-2 flex flex-col items-center justify-start text-gray-custom-3">
+        <span class="text-sm font-bold">
+            {{ $event->formatted_start_date }}
+        </span>
+        <span class="text-sm">
+            {{ $event->formatted_start_time }} - {{ $event->formatted_end_time }}
+        </span>
+    </div>
 
-        {{-- Actions --}}
-        <div class="flex items-center gap-3">
-            @can('view', $event)
-                <x-table-action-button-view />
-            @endcan
+    <div class="col-span-2"></div>
 
-            @can('update', $event)
-                <x-table-action-button-edit />
-            @endcan
+    {{-- User Owner --}}
+    <div class="col-span-2 flex items-center">
+        <x-user-table-data :user="$event->user" />
+    </div>
 
-            @can('delete', $event)
-                <x-table-action-button-delete wire:click='selectCustomerForDelete({{ $event->id }})' />
-            @endcan
-        </div>
+    {{-- Actions --}}
+    <div class="col-span-2 flex items-center justify-end gap-3">
+        @can('view', $event)
+            <x-table-action-button-view />
+        @endcan
+
+        @can('update', $event)
+            <x-table-action-button-edit />
+        @endcan
+
+        @can('delete', $event)
+            <x-table-action-button-delete wire:click='selectCustomerForDelete({{ $event->id }})' />
+        @endcan
     </div>
 
 </div>
