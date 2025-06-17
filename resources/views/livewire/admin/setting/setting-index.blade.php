935c219655364bd51148b4a74bcaf95e69d06e7c
pietro-giordano <giopi87@gmail.com>
2025-06-17 14:22:46 +0200

    fix pages

--- resources/views/livewire/admin/setting/setting-index.blade.php
+++ resources/views/livewire/admin/setting/setting-index.blade.php
@@ -1,7 +1,9 @@
 <div>
     <x-page-title label="Impostazioni" class="mt-1" />
 
-    <x-card>
-        <x-card-header label="Impostazioni" />
+    <x-card class="max-w-3xl mx-auto">
+        <x-card-header class="mb-6" label="Gestione dei valori dei menù a tendina" />
+
+
     </x-card>
 </div>
