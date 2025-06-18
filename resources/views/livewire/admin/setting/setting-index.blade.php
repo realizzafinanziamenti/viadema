c4a47ee93535e4a85d0d327e6ad179f16183bd59
pietro-giordano <giopi87@gmail.com>
2025-06-18 12:37:00 +0200

    update setting index

--- resources/views/livewire/admin/setting/setting-index.blade.php
+++ resources/views/livewire/admin/setting/setting-index.blade.php
@@ -6,8 +6,17 @@
         {{-- Product Subtype Manager --}}
         <livewire:admin.setting.product-subtype-manager />
 
+        {{-- Installment Manager --}}
+        <livewire:admin.setting.installment-manager />
+
+        {{-- Financial Table Manager --}}
+        <livewire:admin.setting.financial-table-manager />
+
         {{-- Insurance Manager --}}
         <livewire:admin.setting.insurance-manager />
+
+        {{-- Customer Type Manager --}}
+        <livewire:admin.setting.customer-type-manager />
 
     </div>
 </div>
