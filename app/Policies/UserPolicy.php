f91ad94fde7ea6420d7cb89607c5d0a39c041bc5
pietro-giordano <giopi87@gmail.com>
2025-06-20 15:52:47 +0200

    rename team member policy in user policy

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -5,7 +5,7 @@ namespace App\Policies;
 use App\Models\User;
 use Illuminate\Auth\Access\Response;
 
-class TeamMemberPolicy
+class UserPolicy
 {
     /**
      * Determine whether the user can view any models.
