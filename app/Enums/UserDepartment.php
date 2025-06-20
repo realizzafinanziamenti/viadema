a8383a5010c3c0534af5b11a5d11a7cca4840be4
pietro-giordano <giopi87@gmail.com>
2025-06-20 16:46:03 +0200

    user department enum class

---
+++ app/Enums/UserDepartment.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Enums;
+
+enum UserDepartment: string
+{
+    case DIRECT_PRODUCTION = 'direct-production';
+    case INDIRECT_PRODUCTION = 'indirect-production';
+    case CALL_CENTER = 'call-center';
+    case CONSULTANT = 'consultant';
+    case EXTERNAL = 'external';
+    case OBSERVER = 'observer';
+
+    public function gettRole(): string
+    {
+        return match ($this) {
+            self::DIRECT_PRODUCTION => 'team_member',
+            self::INDIRECT_PRODUCTION => 'team_member',
+            self::CALL_CENTER => 'team_member',
+            self::CONSULTANT => 'team_member',
+            self::EXTERNAL => 'team_member',
+            self::OBSERVER => 'observer',
+        };
+    }
+
+    public function getLabelText(): string
+    {
+        return match ($this) {
+            self::DIRECT_PRODUCTION => 'Produzione Diretta',
+            self::INDIRECT_PRODUCTION => 'Produzione Indiretta',
+            self::CALL_CENTER => 'Call Center',
+            self::CONSULTANT => 'Sala Consulente',
+            self::EXTERNAL => 'Collaboratore Esterno',
+            self::OBSERVER => 'Osservatore',
+        };
+    }
+}
