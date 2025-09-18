e61cca1a81979e2ed485797d320574f60179fa1e
pietro-giordano <giopi87@gmail.com>
2025-09-18 11:29:13 +0200

    added lead notes modal in index page

---
+++ resources/views/partials/lead/lead-notes-modal.blade.php
@@ -0,0 +1,19 @@
+<x-modal name="lead-notes" maxWidth="lg">
+    <div class="flex flex-col">
+        <x-modal-header label="Note lead {{ $selectedLead?->id }}" class="mb-6" />
+
+        <div class="flex flex-col gap-1.5 col-span-2">
+            <flux:label>Note</flux:label>
+            <x-display-textarea value="{{ $selectedLead?->notes }}" />
+        </div>
+
+        {{-- Button --}}
+        <div class="flex gap-3 justify-end mt-16">
+            <flux:button variant="primary" type="button" size="sm"
+                x-on:click="$dispatch('close-modal', 'lead-notes')"
+                class="px-10 bg-gray-custom-2 border-gray-custom-2 text-gray-custom-5 hover:bg-gray-custom-3-hover hover:border-gray-custom-3-hover hover:text-white">
+                Chiudi
+            </flux:button>
+        </div>
+    </div>
+</x-modal>
