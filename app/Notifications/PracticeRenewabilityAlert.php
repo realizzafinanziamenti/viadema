2dd38fa5096b54c8420756be82f7b6773db69bb9
pietro-giordano <giopi87@gmail.com>
2025-12-18 17:46:24 +0100

    fix carbon import in notifications

--- app/Notifications/PracticeRenewabilityAlert.php
+++ app/Notifications/PracticeRenewabilityAlert.php
@@ -3,6 +3,7 @@
 namespace App\Notifications;
 
 use App\Models\Practice;
+use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
