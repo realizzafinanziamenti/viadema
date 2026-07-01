058d7448a0477947b2b32d57822f773e8fd3e0a9
CiroFiniello <kirofiniello@outlook.it>
2026-07-01 17:43:50 +0200

    feat: add lead statuses

--- app/Enums/LeadStatus.php
+++ app/Enums/LeadStatus.php
@@ -11,7 +11,14 @@ enum LeadStatus: string
     case FEASIBLE = 'feasible';
     case AWAITING_DOCUMENTATION = 'Attesa documentazione';
     case NOT_INTERESTED = 'Non interessato';
-    case ANTETERMINE = 'Antetermine';
+    case AWAITING_CDS = 'Attesa CDS';
+    case INTERNAL_RENEWAL = 'Rinnovo interno';
+    case EXTERNAL_RENEWAL = 'Rinnovo esterno';
+    case INTERNAL_RENEWAL_NOT_INTERESTED = 'Rinnovo interno N.I.';
+    case EXTERNAL_RENEWAL_NOT_INTERESTED = 'Rinnovo esterno N.I.';
+    case UNREACHABLE = 'Irreperibile';
+    case NOT_ANSWERING = 'Non risponde';
+    case INTERESTED = 'Interessato';
 
     /**
      * Get the display label for the current lead status
@@ -26,7 +33,14 @@ enum LeadStatus: string
             self::FEASIBLE => 'Fattibile',
             self::AWAITING_DOCUMENTATION => 'Attesa Doc',
             self::NOT_INTERESTED=> 'Non interessato',
-            self::ANTETERMINE=> 'Antetermine'
+            self::AWAITING_CDS => 'Attesa CDS',
+            self::INTERNAL_RENEWAL => 'Rinnovo interno',
+            self::EXTERNAL_RENEWAL => 'Rinnovo esterno',
+            self::INTERNAL_RENEWAL_NOT_INTERESTED => 'Rinnovo interno N.I.',
+            self::EXTERNAL_RENEWAL_NOT_INTERESTED => 'Rinnovo esterno N.I.',
+            self::UNREACHABLE => 'Irreperibile',
+            self::NOT_ANSWERING => 'Non risponde',
+            self::INTERESTED => 'Interessato',
         };
     }
 
@@ -41,10 +55,17 @@ enum LeadStatus: string
             self::TO_RECONTACT => 'lead-status-to-recontact',
             self::FEASIBLE => 'lead-status-feasible',
             self::IN_NEGOTIATION => 'lead-status-in-negotiation',
-            self::IN_NEGOTIATION => 'lead-status-in-negotiation',
             self::AWAITING_DOCUMENTATION=>'lead-status-in-negotiation',
             self::NOT_INTERESTED=> 'lead-status-not-feasible',
-            self::ANTETERMINE=>'awaiting-renew'
+            self::AWAITING_CDS => 'lead-status-in-negotiation',
+            self::INTERNAL_RENEWAL => 'lead-status-to-recontact',
+            self::EXTERNAL_RENEWAL => 'lead-status-to-recontact',
+            self::INTERNAL_RENEWAL_NOT_INTERESTED => 'lead-status-in-negotiation',
+            self::EXTERNAL_RENEWAL_NOT_INTERESTED => 'lead-status-in-negotiation',
+            self::UNREACHABLE => 'lead-status-in-negotiation',
+            self::NOT_ANSWERING => 'lead-status-in-negotiation',
+            self::INTERESTED => 'lead-status-new',
+
         };
     }
 }
